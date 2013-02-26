cebe1927cffb24d11a550b113baf74000d168a7e
conanca <imconan@gmail.com>
2013-02-26 10:37:57 +0800

    抓取脚本增加生成sql文件功能

--- tools/local.py
+++ tools/local.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''抓取 国家统计局网站 上的最新县及县以上行政区划代码，并保存成 json 格式的js文件
+'''抓取国家统计局网站上最新的县及县以上行政区划代码，并保存成 json 格式的js文件(供前端用)和SQL语句(供后端用)
 by Conanca
 '''
 
@@ -13,7 +13,8 @@ var_text = 'xzqh'
 code_text = 'C'
 name_text = 'N'
 sub_text = 'S'
-file_path = 'xzqh.js'
+jsfile_path = 'xzqh.js'
+sqlfile_path = 'xzqh.sql'
 
 def set_proxy(proxy):
     ''' 设置代理服务器 '''
@@ -37,6 +38,14 @@ def crawl_page(xzqhdm_url):
     index_end = content.find("</TBODY></TABLE>")
     content = content[index_start:index_end]
     return content
+
+def creat_item(item_str):
+    ''' 根据字符串创建条目对象 '''
+    code = item_str[item_str.index('lang=EN-US>') + 11:item_str.index('<o:p></o:p></SPAN></P></TD>')]
+    name = item_str[item_str.index('''mso-bidi-font-family: Tahoma">''') + 30:]
+    item = {code_text:code,name_text:name}
+    print item
+    return item
 
 def convert(content):
     ''' 将爬到的内容转换为行政区划 list '''
@@ -77,26 +86,33 @@ def convert(content):
             print 'invaild item string:'+item_str
     return p_list
 
-def creat_item(item_str):
-    ''' 根据字符串创建条目对象 '''
-    code = item_str[item_str.index('lang=EN-US>') + 11:item_str.index('<o:p></o:p></SPAN></P></TD>')]
-    name = item_str[item_str.index('''mso-bidi-font-family: Tahoma">''') + 30:]
-    item = {code_text:code,name_text:name}
-    #print item
-    return item
+def to_sql(p_list):
+    ''' 将行政区划列表转换为SQL语句 '''
+    sql = 'CREATE TABLE T_XZQH(CODE CHAR(6) PRIMARY KEY,  NAME VARCHAR(30),  PARENT_CODE CHAR(6),  TYPE CHAR(1));\nINSERT INTO T_XZQH VALUES\n'
+    for p in p_list:
+        sql += "('"+p[code_text]+"','"+p[name_text]+"',NULL,'p'),\n"
+        for c in p[sub_text]:
+            sql += "('"+c[code_text]+"','"+c[name_text]+"','"+p[code_text]+"','c'),\n"
+            for d in c[sub_text]:
+                sql += "('"+d[code_text]+"','"+d[name_text]+"','"+c[code_text]+"','d'),\n"
+    return sql[:-2]+";"
+ 
 
 def write_to(content,file_path):
-    ''' 将行政区划列表写入指定的js文件中 '''
+    ''' 将字符串写入指定的文件中 '''
     print 'writing...'
     f = open(file_path, 'w')
-    f.write('var {0}={1}'.format(var_text,content))
+    f.write(content)
     f.close()
-    print 'finished!'
+    print 'done!'
    
 if __name__ == '__main__':
-    set_proxy('http://202.84.17.41:8080')
+    #set_proxy('http://192.168.2.59:8080')
     url = get_latest_page()
     content = crawl_page(url)
     p_list = convert(content)
-    content = json.dumps(p_list,ensure_ascii=False,separators=(',',':')).decode('gb18030').encode('utf-8')
-    write_to(content,file_path)
+    content = 'var ' + var_text + ' = ' + json.dumps(p_list,ensure_ascii=False,separators=(',',':')).decode('gb18030').encode('utf-8')
+    write_to(content,jsfile_path)
+    content = to_sql(p_list).decode('gb18030').encode('utf-8')
+    write_to(content,sqlfile_path)
+    print 'finish!'
